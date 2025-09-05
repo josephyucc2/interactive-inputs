6d033acd0175d5e71feefe7a460fa1c86377146d
yu <josephyucc2@gmail.com>
2025-09-06 01:28:34 +0800

    add mac os support

--- invoke-binary.js
+++ invoke-binary.js
@@ -1,29 +1,45 @@
-const process = require('node:process');
-const os = require('node:os');
-const childProcess = require('node:child_process');
+const process = require( 'node:process' );
+const os = require( 'node:os' );
+const childProcess = require( 'node:child_process' );
 
 const PLATFORM = process.platform;
 const CPU_ARCH = os.arch();
 
 
-function chooseBinary() {
-    if ( PLATFORM !== 'linux') {
-        throw new Error('Only linux is supported')
+function chooseBinary ()
+{
+    if ( PLATFORM !== 'linux' && PLATFORM !== 'darwin' )
+    {
+        throw new Error( 'Only linux and macOS are supported' );
     }
 
-    if ( CPU_ARCH !== 'x64' && CPU_ARCH !== 'arm64') {
-        throw new Error('Only x64 and arm64 are supported')
+    if ( CPU_ARCH !== 'x64' && CPU_ARCH !== 'arm64' )
+    {
+        throw new Error( 'Only x64 and arm64 are supported' );
     }
 
-    if (PLATFORM === 'linux' && CPU_ARCH === 'x64') {
-        return `action-amd64`
+    // Linux binaries
+    if ( PLATFORM === 'linux' && CPU_ARCH === 'x64' )
+    {
+        return `action-amd64`;
     }
-    if (PLATFORM === 'linux' && CPU_ARCH === 'arm64') {
-        return `action-arm64`
+    if ( PLATFORM === 'linux' && CPU_ARCH === 'arm64' )
+    {
+        return `action-arm64`;
+    }
+
+    // macOS binaries
+    if ( PLATFORM === 'darwin' && CPU_ARCH === 'x64' )
+    {
+        return `action-darwin-amd64`;
+    }
+    if ( PLATFORM === 'darwin' && CPU_ARCH === 'arm64' )
+    {
+        return `action-darwin-arm64`;
     }
 }
 
-const binary = chooseBinary()
-const mainScript = `${__dirname}/dist/${binary}`
-const spawnSyncReturns = childProcess.spawnSync(mainScript, { stdio: 'inherit' })
-process.exit(spawnSyncReturns.status ?? 0)
+const binary = chooseBinary();
+const mainScript = `${ __dirname }/dist/${ binary }`;
+const spawnSyncReturns = childProcess.spawnSync( mainScript, { stdio: 'inherit' } );
+process.exit( spawnSyncReturns.status ?? 0 );
